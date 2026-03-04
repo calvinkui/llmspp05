5eb50d05d2110493dea048ec62bfc07b137f8496
ckch <calvinkui@hotmail.com>
2026-03-04 16:28:09 +0800

    updated sh file with parameter expression

--- llm-frontend-python/build.sh
+++ llm-frontend-python/build.sh
@@ -1,2 +1,2 @@
-  docker build --platform linux/arm64 -t darryl1975/llm-frontend-python:latest .        
-  docker push darryl1975/llm-frontend-python:latest 
+  docker build --platform linux/arm64 -t $DOCKERHUB_USERNAME/llm-frontend-python:latest .        
+  docker push $DOCKERHUB_USERNAME/llm-frontend-python:latest 
